499e588dfe43dc4f82f686d860287967772842f5
bed-boy <123461997+viskassss@users.noreply.github.com>
2023-10-29 20:01:45 +0200

    dz1

---
+++ stule.js
@@ -0,0 +1,16 @@
+// // теоретичне питання 1
+// // перемінну в JS можно оголосити через let або const nа var
+// питання 2
+// функція prompt  - корисиувач повинен дати увести відповідь
+// confirm - буде вікно з питанням та кнопкою ок або скасувати
+// завдання 2
+const admin = 'Vova'
+const name = admin 
+console.log(admin)
+
+const days=2
+const seconddays = 86400 
+console.log(days*seconddays)
+
+result = prompt('скільки вам років?')
+console.log (result)
